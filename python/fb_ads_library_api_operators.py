c4eef5c00aa04eda698de0f74542647614d8e10f
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2024-07-31 14:34:49 -0700

    use csv.writer to write csv, and write as we go
    Writing each ad to the csv file as we get it instead of waiting until
    we have all the ads.

--- python/fb_ads_library_api_operators.py
+++ python/fb_ads_library_api_operators.py
@@ -6,9 +6,10 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+from collections import Counter
+import csv
 import datetime
 import json
-from collections import Counter
 import time
 
 
@@ -26,6 +27,7 @@ def get_operators():
         "save_to_csv": save_to_csv,
         "start_time_trending": count_start_time_trending,
     }
+
 
 def output_file_path(keyword, output_path, file_extension):
     # timestamp the output file so we don't inadvertently overwrite an old file
@@ -75,7 +77,7 @@ def save_to_file(ad_archives_generator, args, output_path, is_verbose=False):
 
 def save_to_csv(ad_archives_generator, args, output_path, fields, is_verbose=False):
     """
-    Save all retrieved ad_archives to the output file. Each ad_archive will be
+    Save all retrieved ad_archives to the output file. Each individual ad will be
     stored as a row in the CSV
     Accept one parameter:
         output_keyword: this keyword will be used to tag written files
@@ -83,40 +85,51 @@ def save_to_csv(ad_archives_generator, args, output_path, fields, is_verbose=Fal
     if len(args) != 1:
         raise Exception("save_to_csv action takes 1 argument: output_keyword")
 
-    delimiter = ","
-    total_count = 0
-    output = fields + "\n"
+    headers = fields.split(",")
+    count = 0
 
     output_path = output_file_path(args[0], output_path, ".csv")
 
-    for ad_archives in ad_archives_generator:
-        total_count += len(ad_archives)
-        if is_verbose:
-            print("Items processed: %d" % total_count)
-        for ad_archive in ad_archives:
-            for field in list(fields.split(delimiter)):
-                if field in ad_archive:
-                    value = ad_archive[field]
-                    if (type(value) is list and type(value[0]) is dict) or type(value) is dict:
-                        value = json.dumps(value)
-                    elif type(value) is list:
-                        value = delimiter.join(value)
-                    elif type(value) is int:
-                        value = str(value)
-                    output += (
-                        '"' + value.replace("\n", "").replace('"', "") + '"' + delimiter
-                    )
-                else:
-                    output += delimiter
-            output = output.rstrip(",") + "\n"
+    with open(output_path, "w", newline="") as csvfile:
+        csv_writer = csv.writer(
+            csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
 
-    with open(output_path, "w") as csvfile:
-        csvfile.write(output)
+        # write the header row
+        csv_writer.writerow(headers)
 
-    print("Successfully wrote data to file: %s" % output_path)
+        for ad_archives in ad_archives_generator:
+            count += len(ad_archives)
+            if is_verbose:
+                print("Items processed: %d" % count)
+            for data in ad_archives:
+                output_row = []
+                for field in list(headers):
+                    if field in data:
+                        value = data[field]
+                        string_value = ""
+                        is_json = (
+                            type(value) is list and type(value[0]) is dict
+                        ) or type(value) is dict
+                        if is_json:
+                            string_value = json.dumps(value)
+                        elif type(value) is list:
+                            string_value = ",".join(value)
+                        else:
+                            string_value = str(value)
+
+                        output_row.append(string_value)
+                    else:
+                        output_row.append(None)
+
+                csv_writer.writerow(output_row)
+
+    print(f"Successfully wrote {count} lines of data to {output_path}")
 
 
-def count_start_time_trending(ad_archives_generator, args, output_path, is_verbose=False):
+def count_start_time_trending(
+    ad_archives_generator, args, output_path, is_verbose=False
+):
     """
     output the count trending of ads by start date;
     Accept one parameter:
